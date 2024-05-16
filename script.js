62301d5c991d9381cdc3ed46cc7f3dac62f54caa
Tahiat <164024032+TahiatXCoder@users.noreply.github.com>
2024-05-16 22:12:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     // Check if username and password are correct
     if (username === "Tahiat" && password === "Sam") {
         // Redirect to the dashboard page
-        window.location.href = "../Dahsboard/index.html";
+        window.location.href = "../Dahsboard/dash.html";
     } else {
         alert("Incorrect username or password. Please try again.");
     }
