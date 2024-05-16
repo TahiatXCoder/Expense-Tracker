20b9ef319039364ddd63e4155e255ab6a1b7a016
Tahiat <164024032+TahiatXCoder@users.noreply.github.com>
2024-05-16 22:10:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var username = document.getElementById("name").value;
+    var password = document.getElementById("pass").value;
+
+    // Check if username and password are correct
+    if (username === "Tahiat" && password === "Sam") {
+        // Redirect to the dashboard page
+        window.location.href = "../Dahsboard/index.html";
+    } else {
+        alert("Incorrect username or password. Please try again.");
+    }
+});
